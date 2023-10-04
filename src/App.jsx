4608190c5c2db6bed2fd7478e6e22d55e5a92a98
ronald sarmiento <ronisarmont@gmail.com>
2023-10-03 23:36:18 -0400

    Nombre y la imagen de los pokemons con fetch aPi a api pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ import {useEffect} from 'react';
 
 const App = () => {
   const [pokeId, setPokeID] = useState(1);
-  const [pokeName, setPokeName] = useState();
 
   useEffect(() => {
     getEvolutions(pokeId);
@@ -24,7 +23,28 @@ const App = () => {
       `https://pokeapi.co/api/v2/evolution-chain/${id}/`
     );
     const data = await response.json();
-    setPokeName(data.chain.species.name);
+    let pokemonEvoArray = [];
+    let pokemonLvl1 = data.chain.species.name;
+    let pokemonLvl1Img = await getPokemonimages(pokemonLvl1);
+    pokemonEvoArray.push([pokemonLvl1, pokemonLvl1Img]);
+
+    if (data.chain.evolves_to[0].length !== 0) {
+      let pokemonLvl2 = data.chain.evolves_to[0].species.name;
+      let pokemonLvl2Img = await getPokemonimages(pokemonLvl2);
+      pokemonEvoArray.push([pokemonLvl2, pokemonLvl2Img]);
+      if (data.chain.evolves_to[0].evolves_to.length !== 0) {
+        let pokemonLvl3 = data.chain.evolves_to[0].evolves_to[0].species.name;
+        let pokemonLvl3Img = await getPokemonimages(pokemonLvl3);
+        pokemonEvoArray.push([pokemonLvl3, pokemonLvl3Img]);
+        console.log(pokemonEvoArray);
+      }
+    }
+  };
+
+  const getPokemonimages = async (name) => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}/`);
+    const data = await response.json();
+    return data.sprites.other['official-artwork'].front_default;
   };
 
   const handleClickPrev = () => {
@@ -41,7 +61,7 @@ const App = () => {
 
       <div className='buttons-container'>
         <Button handleClick={handleClickPrev} icon={<TiArrowLeftOutline />} />
-        {pokeId}-{pokeName}
+
         <Button handleClick={handleClickNext} icon={<TiArrowRightOutline />} />
       </div>
     </>
